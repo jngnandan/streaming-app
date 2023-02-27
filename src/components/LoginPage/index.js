249abba587ec070ec695b0f2722f64fde42e7ea2
jngnandan <jngnandan@gmail.com>
2023-02-27 16:35:24 +0000

    jwt token added

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -2,28 +2,75 @@
 import React from 'react'
 import './index.css'
 
-import { Redirect, Navigate, useNavigate } from 'react-router-dom'
+import { Redirect, Navigate, useNavigate, useHistory } from 'react-router-dom'
+import { useState } from 'react'
+
+import Cookies from 'js-cookie'
 
 export default function LoginPage() {
+    const [username, setUsername] = useState('rahul')
+    const [password, setPassword] = useState('rahul@2021')
+    const [error, setError] = useState('')
 
     let navigate = useNavigate()
 
+    const onSubmitSuccess = jwtToken => {
+      const {history} = this.props
+  
+      Cookies.set('jwt_token', jwtToken, {
+        expires: 30,
+        path: '/',
+      })
+      history.replace('/profilepage')
+      const {username, password} = this.state
+      localStorage.setItem('username', username)
+      localStorage.setItem('password', password)
+    }
+  
+    const onSubmitFailure = errorMsg => {
+      this.setState({showError: true, errorMsg})
+    }
+  
+    const onSubmitForm = async event => {
+      event.preventDefault()
+      const userDetails = {username, password}
+      const url = 'https://apis.ccbp.in/login'
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(userDetails),
+      }
+      const response = await fetch(url, options)
+      const data = await response.json()
+      if (response.ok === true) {
+        this.onSubmitSuccess(data.jwt_token)
+        console.log(data.jwt_token)
+      } else {
+        this.onSubmitFailure(data.error_msg)
+      }
+    }
+
+
+    // const jwtToken = Cookies.get('jwt_token')
+
+    // if (jwtToken !== undefined) {
+    //   return <Navigate to='/' />
+    // }
   
 
   return (
     <div className='boxscreen'>
-      <div className='boxing'>
+      <form onSubmit={onSubmitForm} className='boxing'>
       <h1 className='heading'>Login</h1>
         <label id='username'>
           <p className='text'>USERNAME</p>
-          <input type='text' id='username' className='input'/>
+          <input onChange={(e) => setUsername(e.target.value)} value={username} type='text' htmlFor='username' className='input'/>
         </label>
         <label id='password'>
           <p className='text'>PASSWORD</p>
-          <input type='password' id='password' className='input'/>
+          <input onChange={(e) => setPassword(e.target.value)} value={password} type='password' htmlFor='password' className='input'/>
         </label>
-      <button className='buttonStyle' onClick={()=> navigate('/', {replace:true})}>Login</button>
-      </div>
+      <button className='buttonStyle' onClick={()=> console.log(username)}>Login</button>
+      </form>
       
     </div>
   )
